c021ff88a79840f56aae1f21868fcdf00ae7ee19
dalikim <dalikim@parti.xyz>
2019-10-04 09:32:53 +0900

    우리가 하는 일 목록 페이지가 보입니다

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
+  CATEGORY = { 
+    demos: '기관&middot시민 참여 플랫폼',
+    campaigns: '시민주도 캠페인',
+    org: '시민자치 커뮤니티',
+    soft: '디지털 솔루션'
+  }
   def home
+  end
+
+  def what_we_do
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get :what_we_do, to: 'pages#what_we_do'
 end
