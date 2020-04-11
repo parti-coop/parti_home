ba30d2ab1f95017f5b92858b7f24072723933175
dalikim <dalikim@parti.xyz>
2020-04-11 23:57:40 +0900

    협업문의 알림 메일의 제목과 본문에 오류를 바로 잡습니다  #72

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def new_contact
     @contact = params[:contact]
-    mail(to: 'all@parti.coop', subject: "[도킹 요청][#{@contact.contact_org}] #{@contact.project_subject}")
+    mail(to: 'all@parti.coop', subject: "[도킹 요청][#{@contact.contact_org.presence || @contact.contact_manager.presence}] #{@contact.project_subject}")
   end
 end
